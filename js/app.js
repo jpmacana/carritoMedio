44ff70494fbc3b9e1bc12235b5af5433c73cd6ff
jpmacana <jpsenatra@gmail.com>
2021-11-15 16:27:24 -0300

    Terminamos el Mercadito Mediano

--- js/app.js
+++ js/app.js
@@ -1,44 +1,122 @@
 const carrito = document.querySelector("#carrito");
 const template = document.querySelector("#template");
+const footer = document.querySelector("#footer");
+const templateFooter = document.querySelector("#templateFooter");
 const fragment = document.createDocumentFragment();
-const agregar = document.querySelectorAll(".card button");
+let carritoObjeto = [];
 
-const carritoArray = [];
+// Delegación de eventos:
+document.addEventListener("click", (e) => {
+    // console.log(e);
+    // console.log(e.target.dataset.fruta);
+    // console.log(e.target.matches(".card button"));
+    if (e.target.matches(".card button")) {
+        agregarCarrito(e);
+    }
+
+    // console.log(e.target.matches(".list-group-item .btn-success"));
+    if (e.target.matches("#carrito .list-group-item .btn-success")) {
+        btnAumentar(e);
+    }
+
+    // console.log(e.target.matches(".list-group-item .btn-danger"));
+    if (e.target.matches("#carrito .list-group-item .btn-danger")) {
+        btnDisminuir(e);
+    }
+});
 
 const agregarCarrito = (e) => {
+    // console.log(e.target.dataset);
     const producto = {
         titulo: e.target.dataset.comida,
         id: e.target.dataset.comida,
         cantidad: 1,
+        precio: parseInt(e.target.dataset.precio),
     };
 
     // buscamos el indice
-    const index = carritoArray.findIndex((item) => item.id === producto.id);
+    const index = carritoObjeto.findIndex((item) => item.id === producto.id);
 
     // si no existe empujamos el nuevo elemento
     if (index === -1) {
-        carritoArray.push(producto);
+        carritoObjeto.push(producto);
     } else {
         // en caso contrario aumentamos su cantidad
-        carritoArray[index].cantidad++;
+        carritoObjeto[index].cantidad++;
     }
 
-    console.log(carritoArray);
-
+    // console.log(carritoArray);
     pintarCarrito();
 };
 
-agregar.forEach((boton) => boton.addEventListener("click", agregarCarrito));
-
-const pintarCarrito = (array) => {
+const pintarCarrito = () => {
     carrito.textContent = "";
 
     // recorremos el carrito y pintamos elementos:
-    carritoArray.forEach((item) => {
+    carritoObjeto.forEach((item) => {
         const clone = template.content.cloneNode(true);
-        clone.querySelector(".lead").textContent = item.titulo;
+        clone.querySelector(".text-white .lead").textContent = item.titulo;
         clone.querySelector(".rounded-pill").textContent = item.cantidad;
+        clone.querySelector("div .lead span").textContent =
+            item.precio * item.cantidad;
+        clone.querySelector(".btn-success").dataset.id = item.id;
+        clone.querySelector(".btn-danger").dataset.id = item.id;
         fragment.appendChild(clone);
     });
     carrito.appendChild(fragment);
+    
+    pintarFooter();
 };
+
+
+const pintarFooter = () => {
+    footer.textContent = "";
+
+    const total = carritoObjeto.reduce(
+        (acc, current) => acc + current.precio * current.cantidad,
+        0
+    );
+
+    // console.log(total);
+
+    const clone = templateFooter.content.cloneNode(true);
+    clone.querySelector("p span").textContent = total;
+
+    // fragment.appendChild(clone);
+    footer.appendChild(clone);
+};
+
+
+const btnAumentar = (e) => {
+
+    //console.log("Me diste click 😁", e.target.dataset.id)
+
+    carritoObjeto = carritoObjeto.map(item => {
+if (item.id === e.target.dataset.id) {
+    item.cantidad++;
+}
+return item;
+    })
+
+    pintarCarrito();
+
+}
+
+const btnDisminuir = (e) => {
+//console.log("Me diste click 😁", e.target.dataset.id)
+carritoObjeto = carritoObjeto.filter(item => {
+    if (item.id === e.target.dataset.id) {    
+        if (item.cantidad > 0) {
+        item.cantidad--;        
+            if (item.cantidad === 0) return 
+                return item 
+            }
+            } else {return item }
+
+            })
+ pintarCarrito();
+};
+
+
+
+
